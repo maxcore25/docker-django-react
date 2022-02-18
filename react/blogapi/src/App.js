74b869d49ed27899e1e5e8ed8e9c01f5ab3b7e3c
maxcore25 <mkorobov2002@mail.ru>
2022-02-18 15:25:19 +0300

    Port changed and Nginx Proxy set

--- react/blogapi/src/App.js
+++ react/blogapi/src/App.js
@@ -4,26 +4,26 @@ import Posts from './components/Posts';
 import PostLoadingComponent from './components/PostLoading';
 
 function App() {
-	const PostLoading = PostLoadingComponent(Posts);
-	const [appState, setAppState] = useState({
-		loading: false,
-		posts: null,
-	});
+  const PostLoading = PostLoadingComponent(Posts);
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
 
-	useEffect(() => {
-		setAppState({ loading: true });
-		const apiUrl = `http://127.0.0.1:8000/api/`;
-		fetch(apiUrl)
-			.then((data) => data.json())
-			.then((posts) => {
-				setAppState({ loading: false, posts: posts });
-			});
-	}, [setAppState]);
-	return (
-		<div className="App">
-			<h1>Latest Posts</h1>
-			<PostLoading isLoading={appState.loading} posts={appState.posts} />
-		</div>
-	);
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://127.0.0.1/api/`;
+    fetch(apiUrl)
+      .then(data => data.json())
+      .then(posts => {
+        setAppState({ loading: false, posts: posts });
+      });
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <h1>Latest Posts</h1>
+      <PostLoading isLoading={appState.loading} posts={appState.posts} />
+    </div>
+  );
 }
 export default App;
